7d53390bacaadb8dd777010df9d1233f990d73de
davidecrs <davidecrs92@gmail.com>
2023-07-10 11:55:06 +0200

    Fixed assert on data.frame

--- R/polygonplot.R
+++ R/polygonplot.R
@@ -59,7 +59,7 @@ polygonplot <- function(df, shape,
   
   # Check params type
   checkmate::assertInt(shape, lower = 3, upper = 6)
-  checkmate::assertDataFrame(df, ncols = 1+shape, col.names = "named")
+  checkmate::assertDataFrame(df, min.cols = 1+shape, col.names = "named")
   
   checkmate::assertDouble(extra, lower = 0, upper = 1)
   checkmate::assertDouble(alpha, lower = 0, upper = 1)
