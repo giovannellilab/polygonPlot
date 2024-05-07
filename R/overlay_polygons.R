65be06c2dbb7265e712b58a3486a1ce28cadb4b3
davidecrs <davidecrs92@gmail.com>
2024-05-07 13:10:51 +0200

    Added function to overlap polygons. Close #6

---
+++ R/overlay_polygons.R
@@ -0,0 +1,56 @@
+
+#' Overlay different polygon plots on the same axis
+#'
+#' @param plot_list is a list object containing a series of polygon plots 
+#' (ggplot2 object). All the polygon plot should be drawn on the same axis range
+#' @param label_list is a vector of strings. Must have the same length of 
+#' `plot_list` object
+#'
+#' @return a ggplot2 object
+#' @export
+#'
+#' @import checkmate
+#' @import ggplot2
+overlay_polygons <- function(plot_list, label_list) {
+  checkmate::assertList(plot_list, types = "ggplot")
+  checkmate::assertCharacter(label_list, len = length(plot_list), 
+                             null.ok = FALSE, any.missing = FALSE)
+  
+  first_plot <- plot_list[[1]]
+  # Get the ggplot build from the first plot
+  gb1 <- ggplot2::ggplot_build(first_plot)
+  
+  # Removing Geom Points from the first plot. Now Polygon will be the first layer
+  first_plot$layers[[1]] <- NULL
+  # Removing Geom Polygon from the first plot. 
+  first_plot$layers[[1]] <- NULL
+  
+  m <- gb1$data[[2]]
+  m$plot_labels <- label_list[[1]]
+  
+  for(i in seq.int(from=2, to=length(plot_list))){
+    gbX <- ggplot2::ggplot_build(plot_list[[i]])
+    gbX$data[[2]]$plot_labels <- label_list[[i]]
+    
+    # Append polygon and point info
+    m <- rbind(m, gbX$data[[2]])
+  }
+  
+  fil <- as.character(m$fill)
+  names(fil) <- as.character(m$plot_labels)
+  
+  al = m$alpha
+  names(al) <- as.character(m$plot_labels)
+  
+  col <- as.character(m$colour)
+  names(col) <- as.character(m$plot_labels)
+  
+  master_plot <- first_plot +
+    ggplot2::geom_polygon(data=m, aes(x, y, fill=plot_labels, alpha = plot_labels, colour = plot_labels), 
+                          linewidth = unique(m$linewidth)) +
+    ggplot2::scale_fill_manual(values = fil) + 
+    ggplot2::scale_alpha_manual(values = al) + 
+    ggplot2::scale_colour_manual(values = col) 
+  
+  return(master_plot)
+}
