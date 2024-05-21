973d691a3bd71e21cb9d4cda84809b0f6e5e9bc3
guillecg <guillemcg92@gmail.com>
2024-05-21 13:20:45 +0200

    Add test for .get_perimeter

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -22,6 +22,31 @@ test_that(".get_lengths returns correct values", {
   )
 })
 
+test_that(".get_perimeter returns correct values", {
+  
+  df_coord = read.csv(test_path("testdata", "df_coord.csv"))
+  
+  df_result = data.frame()
+  for (pol_shape in unique(df_coord$shape)) {
+    shape_df = df_coord %>%
+      dplyr::filter(shape == pol_shape) %>%
+      select(-"shape")
+    shape_df = data.frame(
+      perimeter=.get_perimeter(shape_df),
+      shape=pol_shape
+    )
+    df_result = df_result %>% bind_rows(shape_df)
+  }
+  
+  df_expected = read.csv(test_path("testdata", "df_coord_perimeter_area.csv"))
+  df_expected = df_expected %>% dplyr::select(-"area")
+  
+  expect_equal(
+    object=df_result,
+    expected=df_expected
+  )
+})
+
 test_that(".collapse_polygon returns correct values", {
   
   df_coord = read.csv(test_path("testdata", "df_coord.csv"))
