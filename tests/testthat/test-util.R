0f71a4f66472e5b741087393b268c80b2f811c8d
guillecg <guillemcg92@gmail.com>
2024-05-21 13:18:11 +0200

    Add test for .collapse_polygon

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -21,3 +21,25 @@ test_that(".get_lengths returns correct values", {
     expected=df_expected
   )
 })
+
+test_that(".collapse_polygon returns correct values", {
+  
+  df_coord = read.csv(test_path("testdata", "df_coord.csv"))
+  
+  df_result = data.frame()
+  for (pol_shape in unique(df_coord$shape)) {
+    shape_df = df_coord %>%
+      dplyr::filter(shape == pol_shape) %>%
+      select(-"shape")
+    shape_df = .collapse_polygon(shape_df)
+    shape_df$shape = pol_shape
+    df_result = df_result %>% bind_rows(shape_df)
+  }
+  
+  df_expected = read.csv(test_path("testdata", "df_coord_collapse.csv"))
+  
+  expect_equal(
+    object=df_result,
+    expected=df_expected
+  )
+})
