25da980e9c9e3743fdcefe7b16fb5566b9c7f9ab
davidecrs <davidecrs92@gmail.com>
2025-10-21 19:41:27 +0200

    Updated tests

--- tests/testthat/test-ggplot2_object.R
+++ tests/testthat/test-ggplot2_object.R
@@ -5,5 +5,5 @@ test_that("polygonplot function return a ggplot2 object", {
   df <- read.csv(system.file(file.path("extdata", "example.csv"), 
                              package="polygonPlot"))
   plot <- polygonplot(df, shape = 4)
-  expect_true(is.ggplot(plot))
+  expect_true(is_ggplot(plot))
 })
