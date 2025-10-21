96293dc60e284271b8b3fc58475207da75b8ebd3
davidecrs <davidecrs92@gmail.com>
2025-10-21 19:03:26 +0200

    Added mean line color option and fixed draw lines

--- R/mean-lines.R
+++ R/mean-lines.R
@@ -8,7 +8,6 @@
 #' 
 #' @import dplyr
 .get_mean_coords = function(df) {
-  
   mean_df = data.frame()
   
   # WARNING: this code assumes the order of the points is correct!
@@ -19,34 +18,33 @@
       )
   }
   
-  return(mean_df)
+  # Close polygon by repeating first point
+  df_closed <- rbind(mean_df, mean_df[1, ])
+  
+  return(df_closed)
 }
 
-#' Plots the mean polygon
+#' Plots the mean lines
 #' 
 #' @param df Data frame containing the coordinates in two columns: x and y
 #' 
-#' @return The mean polygon as a `ggplot2` object.
+#' @return The mean lines as a `ggplot2` object.
 #' 
 #' @seealso [polygonPlot::.get_perimeter()]
 #' 
 #' @import checkmate
 #' @import ggplot2
-.draw_mean_polygon = function(df) {
+.draw_mean_lines = function(df, mean_line_color) {
   checkmate::assertDataFrame(x=df, col.names="named", ncols=2)
   
   # Get mean coordinates from original ones
   mean_df = .get_mean_coords(df)
   
-  p = ggplot2::geom_polygon(
-      data=mean_df,
-      aes(x=get("x"), y=get("y")),
-      fill="darkgrey",
-      colour="darkgrey",
-      alpha=0.0,
-      linetype=1,
-      linewidth=0.5
-    )
+  p <- ggplot2::geom_path(data=mean_df,
+                            aes(x=x, y=y),
+                            colour=mean_line_color,
+                            linetype=1,
+                            linewidth=0.5)
   
   return(p)
 }
